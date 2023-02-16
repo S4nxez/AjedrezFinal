efd35d54605421648f8c57be9c093045e11b066e
Nachetto <115470440+Nachetto@users.noreply.github.com>
2023-02-16 09:53:21 +0100

    seguimos con el enroque

--- src/Figures/King.java
+++ src/Figures/King.java
@@ -48,29 +48,33 @@ public class King extends ChessFigure {
     public boolean movement(Movement mov, Tablero tb) {
         if (mov.esDiagonal()&&(Math.abs(mov.saltoVertical()) + Math.abs(mov.saltoHorizontal()) == 2 ))
             return true;
-        else if (Math.abs(mov.saltoHorizontal())==1 && mov.esHorizontal())
-            return true;
         else if (Math.abs(mov.saltoVertical())==1 && mov.esVertical())
             return true;
-        else if(Math.abs(mov.saltoHorizontal())==2 && mov.esHorizontal() ){//si se mueve 2 porque el enroque esta habilitado...
-            isKingMoved();
-            if (getColor()==true){
-                if (mov.saltoHorizontal()==-2){
-                    //activas un metodo EN TABLERO para que quite la torre izquierda y la ponga una a la derecha del rey (efectuando enrroque(){quitarpieza(torreizquierda);tableroFichas[posicion derecha del rey] = new Rook(true);})
+        else if (mov.esHorizontal()){
+            if (Math.abs(mov.saltoHorizontal())==1)
+                return true;
+            if(mov.saltoHorizontal()==2 || mov.saltoHorizontal()==-3){
+                isKingMoved();
+                if (getColor()==true && tb.deteccionEnroque(mov).charAt(5)==0 /*si el rey negro no se ha movido de su posicion inicial*/){
+                    if (mov.saltoHorizontal()==-3){
+                        //activas un metodo EN TABLERO para que quite la torre izquierda y la ponga una a la derecha del rey (efectuando enrroque(){quitarpieza(torreizquierda);tableroFichas[posicion derecha del rey] = new Rook(true);})
+                    }
+                    if (mov.saltoHorizontal()==2){
+                        //activas un metodo EN TABLERO para que quite la torre derecha y la ponga una a la derecha del rey "" "" ""
+                    }
                 }
-                if (mov.saltoHorizontal()==2){
-                    //activas un metodo EN TABLERO para que quite la torre derecha y la ponga una a la derecha del rey "" "" ""
+                if (getColor()==false && tb.deteccionEnroque(mov).charAt(6)==0 /*si el rey blanco no se ha movido de su posicion inicial*/){
+                    if (mov.saltoHorizontal()==-2){
+                        //activas un metodo EN TABLERO para que quite la torre izquierda y la ponga una a la derecha del rey (efectuando enrroque(){quitarpieza(torreizquierda);tableroFichas[posicion derecha del rey] = new Rook(false);})
+                    }
+                    if (mov.saltoHorizontal()==2){
+                        //activas un metodo EN TABLERO para que quite la torre derecha y la ponga una a la derecha del rey "" "" ""
+                    }
                 }
+                return true;
             }
-            if (getColor()==false){
-                if (mov.saltoHorizontal()==-2){
-                    //activas un metodo EN TABLERO para que quite la torre izquierda y la ponga una a la derecha del rey (efectuando enrroque(){quitarpieza(torreizquierda);tableroFichas[posicion derecha del rey] = new Rook(false);})
-                }
-                if (mov.saltoHorizontal()==2){
-                    //activas un metodo EN TABLERO para que quite la torre derecha y la ponga una a la derecha del rey "" "" ""
-                }
-            }
-            return true;}
+
+        }
         else return false;
     }
 }
--- src/Mecanicas/Tablero.java
+++ src/Mecanicas/Tablero.java
@@ -14,13 +14,15 @@ public class Tablero {
     public ChessFigure[][] getTableroFichas() {
         return tableroFichas;
     }
-    public void deteccionEnroque(Movement mov) {
-        int ul=0, ur=0, dl=0, dr=0;//up down left right
+    public String deteccionEnroque(Movement mov) {
+        int ul=0, ur=0, dl=0, dr=0, mt=0, mb=0;//up down left right
         if (tableroFichas[0][0] == null)ul++;
         if(tableroFichas[7][0] == null)ur++;
         if(tableroFichas[0][7] == null)dl++;
         if(tableroFichas[7][7] == null)dr++;
-
+        if(tableroFichas[0][4] == null)mt++;
+        if(tableroFichas[7][4] == null)mb++;
+        return ul+ur+dl+dr+mt+mb+"";
     }
 
     /**
